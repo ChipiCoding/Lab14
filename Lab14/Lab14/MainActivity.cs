0da48636cab585e7d6c0204b63bb6f92916b1f0f
Andres Mauricio Zapata Caro <maurozapata17@gmail.com>
2017-06-27 16:26:54 -0500

    Las 14 - 1 ok

--- Lab14/Lab14/MainActivity.cs
+++ Lab14/Lab14/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using SALLab14;
 
 namespace Lab14
 {
@@ -13,6 +14,28 @@ namespace Lab14
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            Button buttonValidate = FindViewById<Button>(Resource.Id.buttonValidate);
+            buttonValidate.Click += delegate
+            {
+                ValidateAsync();
+            };
+        }
+
+        public async void ValidateAsync()
+        {
+            ServiceClient Service = new ServiceClient();
+            ResultInfo SvcResult = await Service.ValidateAsync(this);
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            AlertDialog alert = builder.Create();
+            alert.SetTitle("Resultado de la verificación");
+            alert.SetIcon(Resource.Drawable.Icon);
+            alert.SetMessage($"{SvcResult.Status}\n{SvcResult.FullName}\n{SvcResult.Token}");
+            alert.SetButton("Ok", (s, ev) => { });
+            alert.Show();
+            //FindViewById<TextView>(Resource.Id.TextViewResult).Text = $"\n{SvcResult.Status.ToString()}\n{SvcResult.Token}";
+            //ValidationData.Result = $"\n{SvcResult.Status.ToString()}\n{SvcResult.Token}";
         }
     }
 }
